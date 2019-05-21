8ddbf876791145bb3184051e718d243baaa92f0e
Thuan Nguyen <235thuan@gmail.com>
2019-05-21 16:20:17 +0700

    nop lan 1

---
+++ bai1.js
@@ -0,0 +1,10 @@
+let name='kacelocom';
+let name1="123abc_";
+let name2="abc123";
+let name3="______";
+let name4="123456";
+let name5=".@";
+let pattern=/^[A-Z_a-z0-9]{6,}$/;
+let result=pattern.test(name1);
+alert(result);
+
